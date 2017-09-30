814b7b3dbc8e0cf1e6642700a93e0238d1fcce4e
Matthew Nestor <mtthwnestor@gmail.com>
2017-09-30 19:53:59 -0400

    Create a threaded server for multiple connections.

---
+++ ClientThread.py
@@ -0,0 +1,26 @@
+from threading import Thread
+
+
+class ClientThread(Thread):
+
+    def __init__(self, connection, address):
+        Thread.__init__(self)
+
+        self.connection = connection
+        self.address = address
+
+        print("[+] New connection from: " + str(address))
+
+    def run(self):
+
+        try:
+            #TODO: Handle each thread response better.
+            while True:
+                data = self.connection.recv(1024).decode()
+                print(str(self.address) + ": " + str(data))
+
+                message = input("> ")
+                self.connection.send(message.encode())
+        except KeyboardInterrupt:
+            print("Closed connection: " + self.address)
+            self.connection.close()
---
+++ ThreadedServer.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python3
+
+import sys
+import socket
+
+import ClientThread
+
+
+class Server:
+
+    # Define some understandable constants
+    IP = socket.AF_INET
+    IP6 = socket.AF_INET6
+    TCP = socket.SOCK_STREAM
+    UDP = socket.SOCK_DGRAM
+
+    def __init__(self, host, port):
+
+        # Please define the listening address and port on initialisation.
+        self.host = host
+        self.port = port
+
+    def run(self):
+
+        try:
+            # Create the socket and bind it to the address and port.
+            my_sock = socket.socket(self.IP, self.TCP)
+            my_sock.bind((self.host, self.port))
+        except socket.error as e:
+            print(e)
+            sys.exit()
+
+        try:
+            while True:
+                my_sock.listen(5)
+                # Enter waiting and accept incoming connection.
+                connection, address = my_sock.accept()
+
+                newthread = ClientThread.ClientThread(connection, address)
+                newthread.start()
+        except KeyboardInterrupt:
+            # Ctrl+C to exit program.
+            print("\nKeyboardInterrupt received. Closing...")
+            connection.close()
+            sys.exit()
+
+        connection.close()
+
+
+if __name__ == "__main__":
+
+    server = Server("127.0.0.1", 5001)
+
+    server.run()
